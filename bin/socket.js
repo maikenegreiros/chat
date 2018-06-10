f9c32daf650ff7edfe4c11584be0274217db73e3
Maike <maike_jbe@hotmail.com>
2018-06-09 21:49:14 -0300

    initial commit

---
+++ bin/socket.js
@@ -0,0 +1,39 @@
+const WebSocketServer = require('websocket').server
+const http = require('http')
+const express = require('express')
+const path = require('path');
+const app = express()
+const server = http.createServer(app)
+const router = require('../routes/index')
+const port = 3000
+
+app.use(express.static(__dirname + '/../public' ));
+app.set('views', path.join(__dirname, '/../views'));
+app.set('view engine', 'pug');
+app.use("/", router)
+
+server.listen(port, function() {
+    console.log(`servidor na porta ${port}`)
+})
+
+const wsServer = new WebSocketServer({httpServer: server})
+
+let clients = []
+let connection
+
+wsServer.on('request', function(request) {
+    console.log(`Conexão de ${request.origin}`)
+    connection = request.accept(null, request.origin)
+    clients.push(connection)
+    console.log(clients.length)
+
+    connection.on('message', function(message) {
+        if (message.type === "utf8") {
+            console.log(message.utf8Data)
+            clients.forEach(client => client.sendUTF(message.utf8Data))
+        }
+    })
+    connection.on('close', function(connection) {
+
+    })
+})
---
+++ public/javascripts/chat.js
@@ -0,0 +1,69 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const port = 3000
+    const connection = new WebSocket(`ws://127.0.0.1:${port}`)
+
+    const container = document.querySelector(".messages-container")
+    const inputName = document.querySelector(".input-name")
+    const buttonName = document.querySelector(".button-name")
+    const messageInput = document.querySelector('.message-input')
+    const buttonSend = document.querySelector(".message-input")
+
+    let user = ""
+
+    let p
+    let pUser
+    let div
+
+    const colors = ["blue", "green", "gray", "red"]
+
+    connection.onopen = () => {
+        console.log("connection is open")
+    }
+    connection.onerror = error => {
+        console.log(error)
+    }
+    connection.onmessage = message => {
+        appendMessage(message)
+    }
+
+    buttonName.addEventListener("click", () => {
+        user = inputName.value
+    })
+    inputName.addEventListener("keydown", e => {
+        if (e.keyCode === 13) {
+            user = inputName.value
+        }
+    })
+
+    buttonSend.addEventListener("click", () => {
+        send("message", messageInput.value)
+    })
+    messageInput.addEventListener("keydown", e => {
+        if (e.keyCode === 13) {
+            send("message", messageInput.value)
+        }
+    })
+
+    const send = (type, data) => {
+        var d = {
+            "type": type,
+            "data": {"user": user, "message": data}
+        }
+        connection.send(JSON.stringify(d))
+        cleanInput(messageInput)
+    }
+    const appendMessage = (data) => {
+        console.log(data)
+        div = document.createElement('div')
+        pUser = document.createElement('p')
+        p = document.createElement('p')
+        container.appendChild(div)
+        div.appendChild(pUser)
+        div.appendChild(p)
+        pUser.appendChild(document.createTextNode(JSON.parse(data.data).data.user))
+        p.appendChild(document.createTextNode(JSON.parse(data.data).data.message))
+    }
+    const cleanInput = input => {
+        input.value = ''
+    }
+})
